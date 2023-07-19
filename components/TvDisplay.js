07cae2674b8da17aedec39a483e2b4c810bba51c
Kamey_ <kamey_@noreply.codeberg.org>
2023-07-19 18:43:42 +0000

    Update 'components/TvDisplay.js'

--- components/TvDisplay.js
+++ components/TvDisplay.js
@@ -1,11 +1,21 @@
-// Import required components
 import Link from "next/link";
 import TvCards from "./TvCards";
 
-// TvDisplay component definition
 const TvDisplay = ({ tv, pageid = 1 }) => {
   // Create an array of page numbers for pagination
   const pagenum = Array.from({ length: 10 }, (_, i) => i + 1);
+
+  // Ensure pageid is a number and defaults to 1 if not
+  pageid = isNaN(pageid) ? 1 : Number(pageid);
+
+  // Ensure pageid is within valid range
+  if (pageid < 1) {
+    pageid = 1;
+  }
+  const maxPage = pagenum.length;
+  if (pageid > maxPage) {
+    pageid = maxPage;
+  }
 
   // Return the JSX for the TvDisplay component
   return (
@@ -21,10 +31,16 @@ const TvDisplay = ({ tv, pageid = 1 }) => {
         {/* Previous page link */}
         <Link
           legacyBehavior
-          href={`/movie/popular/page/\\${Number(pageid) - 1}`}
+          href={`/tv/popular/page/${pageid - 1}`}
           passHref
+          // Disable Previous button when on the first page
+          aria-disabled={pageid === 1}
         >
-          <a className="bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700">
+          <a
+            className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
+              pageid === 1 ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
             &laquo; Prev
           </a>
         </Link>
@@ -33,12 +49,12 @@ const TvDisplay = ({ tv, pageid = 1 }) => {
           <Link
             legacyBehavior
             key={element}
-            href={`/tv/popular/page/\\${element}`}
+            href={`/tv/popular/page/${element}`}
             passHref
           >
             <a
               className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
-                pageid == element ? "bg-gray-700" : ""
+                pageid === element ? "bg-gray-700" : ""
               }`}
             >
               {element}
@@ -48,10 +64,16 @@ const TvDisplay = ({ tv, pageid = 1 }) => {
         {/* Next page link */}
         <Link
           legacyBehavior
-          href={`/tv/popular/page/\${Number(pageid) + 1}`}
+          href={`/tv/popular/page/${pageid + 1}`}
           passHref
+          // Disable Next button when on the last page
+          aria-disabled={pageid === maxPage}
         >
-          <a className="bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700">
+          <a
+            className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
+              pageid === maxPage ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
             Next &raquo;
           </a>
         </Link>
