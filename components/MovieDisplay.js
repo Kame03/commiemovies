2a8a9381c8809df5d729ef911421f503a9010fd5
Kamey_ <kamey_@noreply.codeberg.org>
2023-07-19 18:43:15 +0000

    Update 'components/MovieDisplay.js'

--- components/MovieDisplay.js
+++ components/MovieDisplay.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import TvCards from "./TvCards";
+import MovieCard from "./MovieCards";
 
-const TvDisplay = ({ tv, pageid = 1 }) => {
-  // Create an array of page numbers for pagination
+const MovieDisplay = ({ movie, pageid }) => {
+  // Create an array of page numbers
   const pagenum = Array.from({ length: 10 }, (_, i) => i + 1);
-
   // Ensure pageid is a number and defaults to 1 if not
-  pageid = isNaN(pageid) ? 1 : Number(pageid);
+  pageid = isNaN(pageid) ? 1 : +pageid;
 
   // Ensure pageid is within valid range
   if (pageid < 1) {
@@ -17,21 +16,35 @@ const TvDisplay = ({ tv, pageid = 1 }) => {
     pageid = maxPage;
   }
 
-  // Return the JSX for the TvDisplay component
+  // Render a single pagination item
+  const renderPaginationItem = (element) => (
+    <Link key={element} href={`/movie/popular/page/${element}`} passHref>
+      <a
+        className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
+          pageid === element ? "bg-gray-700" : ""
+        }`}
+      >
+        {element}
+      </a>
+    </Link>
+  );
+
   return (
     <div className="popular-movies md:mx-24">
-      {/* Display a grid of TV cards */}
+      {/* Render movie cards */}
       <div className="flex flex-wrap overflow-hidden sm:-mx-2 pl-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
-        {tv.map((TvCard) => (
-          <TvCards key={TvCard.id} TvCard={TvCard} />
+        {movie?.map((movieCard) => (
+          <MovieCard
+            key={movieCard.id}
+            MovieCard={movieCard}
+            className="justify-center rounded-md"
+          />
         ))}
       </div>
-      {/* Create pagination navigation */}
+      {/* Render pagination */}
       <div className="flex justify-center mt-8 space-x-4 mb-10">
-        {/* Previous page link */}
         <Link
-          legacyBehavior
-          href={`/tv/popular/page/${pageid - 1}`}
+          href={`/movie/popular/page/${pageid - 1}`}
           passHref
           // Disable Previous button when on the first page
           aria-disabled={pageid === 1}
@@ -44,27 +57,9 @@ const TvDisplay = ({ tv, pageid = 1 }) => {
             &laquo; Prev
           </a>
         </Link>
-        {/* Page number links */}
-        {pagenum.map((element) => (
-          <Link
-            legacyBehavior
-            key={element}
-            href={`/tv/popular/page/${element}`}
-            passHref
-          >
-            <a
-              className={`bg-gray-800 text-white rounded-full px-4 py-2 hover:bg-gray-700 ${
-                pageid === element ? "bg-gray-700" : ""
-              }`}
-            >
-              {element}
-            </a>
-          </Link>
-        ))}
-        {/* Next page link */}
+        {pagenum.map(renderPaginationItem)}
         <Link
-          legacyBehavior
-          href={`/tv/popular/page/${pageid + 1}`}
+          href={`/movie/popular/page/${pageid + 1}`}
           passHref
           // Disable Next button when on the last page
           aria-disabled={pageid === maxPage}
@@ -82,5 +77,4 @@ const TvDisplay = ({ tv, pageid = 1 }) => {
   );
 };
 
-// Export TvDisplay component as default
-export default TvDisplay;
+export default MovieDisplay;
